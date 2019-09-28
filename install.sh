b036d70575bdfd5cefd8358c0bc8c873f43042c4
sidey <siraa111@gmail.com>
2019-09-28 22:48:54 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,6 +17,8 @@ sudo systemctl enable nginx.service
 sudo systemctl start nginx.service
 printf "\033[32m Done \033[0m\n"
 printf "\033[32m Installing PHP7.2...\033[0m\n"
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:ondrej/php
 sudo apt install php7.2-fpm php7.2-common php7.2-mbstring php7.2-xmlrpc php7.2-soap php7.$
 sudo systemctl restart nginx.service
 printf "\033[32m Done \033[0m\n\n"
